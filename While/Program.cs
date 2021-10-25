66ac06811d4beb3ab472b7cae24b83ffb65d4553
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-10-25 13:43:07 -0700

    Add files via upload

---
+++ While/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a letter");
+            string Letter = Console.ReadLine();
+            bool isCorrect = Letter == "z";
+
+            while (!isCorrect){
+                switch (Letter)
+                {
+                    case "a":
+                        Console.WriteLine("The letter is not a try again");
+                        Console.WriteLine("Guess a letter");
+                        Letter = Console.ReadLine();
+                        break;
+
+                    case "b":
+                        Console.WriteLine("The letter is not b try again");
+                        Console.WriteLine("Guess a letter");
+                        Letter = Console.ReadLine();
+                        break;
+
+                    case "f":
+                        Console.WriteLine("The letter is not f try again");
+                        Console.WriteLine("Guess a letter");
+                        Letter = Console.ReadLine();
+                        break;
+
+                    case "z":
+                        Console.WriteLine("The letter is z good job!");
+                        isCorrect = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You are wrong try again");
+                        Console.WriteLine("Guess a letter");
+                        Letter = Console.ReadLine();
+                        break;
+
+                }
+
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
