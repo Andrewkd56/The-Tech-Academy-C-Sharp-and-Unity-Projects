6a9f693307cc1d4c9f7a4c478f74a4d79f2a98bb
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-10-25 13:42:42 -0700

    Add files via upload

---
+++ DoWhile/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess my name?");
+            string name = Console.ReadLine();
+            bool isGuessed = name == "Andrew";
+
+            do
+            {
+
+                switch (name)
+                {
+                    case "John":
+                        Console.WriteLine("My name is not John. Try again");
+                        Console.WriteLine("Guess my name?");
+                        name = Console.ReadLine();
+                        break;
+
+                    case "Alex":
+                        Console.WriteLine("My name is not Alex. Try again");
+                        Console.WriteLine("Guess my name?");
+                        name = Console.ReadLine();
+                        break;
+
+                    case "Donald":
+                        Console.WriteLine("My name is not Donald. Try again");
+                        Console.WriteLine("Guess my name?");
+                        name = Console.ReadLine();
+                        break;
+
+                    case "Andrew":
+                        Console.WriteLine("My name is Andrew! Good job!");
+                        isGuessed = true;
+                        
+                        break;
+
+                    default:
+                        Console.WriteLine("You are wrong try again");
+                        Console.WriteLine("Guess my name?");
+                        name = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!isGuessed);
+            Console.ReadLine();
+
+
+        }
+    }
+}
