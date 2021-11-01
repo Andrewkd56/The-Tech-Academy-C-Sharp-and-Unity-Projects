3a3d5b210890fa3f509c3b14940fc579021b1516
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-11-01 11:41:13 -0700

    Add files via upload

---
+++ CallingMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        public int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input:");
+            int line = Convert.ToInt32(Console.ReadLine());
+
+            var operatorObject = new Operator();
+            var result = operatorObject.GetAdd(line);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter input:");
+            line = Convert.ToInt32(Console.ReadLine());
+
+            operatorObject = new Operator();
+            result = operatorObject.GetSubtract(line);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter input:");
+            line = Convert.ToInt32(Console.ReadLine());
+
+            operatorObject = new Operator();
+            result = operatorObject.GetDivide(line);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CallingMethods/ThreeMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Operator
+    {
+        public double GetAdd(int data)
+        {
+            data = data + 4;
+            return data;
+        }
+
+        public double GetSubtract(int data)
+        {
+            data = data - 4;
+            return data;
+        }
+
+        public double GetDivide(int data)
+        {
+            data = data / 1;
+            return data;
+        }
+    }
+}
