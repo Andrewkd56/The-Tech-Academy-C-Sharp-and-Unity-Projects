2a889454ab3f8f890c3b8eef93cfc79574dffc20
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-11-01 13:06:32 -0700

    Add files via upload

---
+++ Main Method/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Math
+    {
+        public double GetAdd(int data)
+        {
+            data = data + 4;
+            return data;
+        }
+
+        public decimal GetMultiply(decimal data)
+        {
+            data = data * 4;
+            return data;
+        }
+
+        public double GetSubtract (int data)
+        {
+            data = data - 4;
+            return data;
+        }
+    }
+}
---
+++ Main Method/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        public int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input:");
+            int line = Convert.ToInt32(Console.ReadLine());
+
+            var operatorObject = new Math();
+            var result = operatorObject.GetAdd(line);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter input:");
+            decimal line1 = Convert.ToDecimal(Console.ReadLine());
+
+            operatorObject = new Math();
+            result = (double)operatorObject.GetMultiply(line1);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter input:");
+            int line2 = Convert.ToInt32(Console.ReadLine());
+
+            operatorObject = new Math();
+            result = (double)operatorObject.GetMultiply(line2);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
