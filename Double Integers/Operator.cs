78bc35ba83fcc52b376ccd1145f5be963e7a8962
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-11-01 21:24:31 -0700

    Add files via upload

---
+++ Double Integers/Operator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Integers
+{
+    class Operator
+    {
+        public int operate(int data, int input = 4)
+        {
+            return data + input;
+        }
+    }
+}
