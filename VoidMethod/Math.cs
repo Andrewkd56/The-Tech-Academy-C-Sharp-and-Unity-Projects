cdda30b7c88a818b192038ab55d422f54369f9a9
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-11-02 20:33:08 -0700

    Add files via upload

---
+++ VoidMethod/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    public class Math
+    {
+        public static void computeSum(int num1, int num2)
+        {
+            int sum = 6;
+            sum = num1 + sum;
+
+            Console.WriteLine(num2);
+            Console.WriteLine("The sum of your first integer is " + sum);
+        }
+    }
+}
---
+++ VoidMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            int int1 = 33;
+            Math.computeSum(int1, 22);
+            Console.ReadLine();
+
+        }
+    }
+}
