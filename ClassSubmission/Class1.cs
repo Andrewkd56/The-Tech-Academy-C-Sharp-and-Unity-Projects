99dff80244f7766d01e14244f50b9d2b9663a757
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-11-03 22:52:09 -0700

    Add files via upload

---
+++ ClassSubmission/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class Class1
+    {
+        private static string msg = "Hello world";
+
+        public static class nestedStaticClass
+        {
+            public static void printMessage()
+            {
+                Console.WriteLine(msg);
+            }
+
+        }
+
+        public static void Division(int int1)
+        {
+            int quotient = int1 / 2;
+
+            Console.WriteLine(int1 + " divided by " + 2 + " equals " + quotient);
+        }
+        public static void Division(decimal int1)
+        {
+            int quotient1 = Convert.ToInt32(int1) / 3;
+
+            Console.WriteLine(quotient1);
+
+        }
+
+        public static void Addition(out int i)
+        {
+            i = 30;
+            i += i;
+        }
+
+    }
+}
+
+    
---
+++ ClassSubmission/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Class1 class1 = new Class1();
+            
+            Console.WriteLine("type in a number");
+            int int1 = Convert.ToInt32(Console.ReadLine());
+
+            Class1.Division(int1);
+
+            decimal myValue1 = 33.3m;
+            Class1.Division(myValue1);
+
+            
+
+
+            int i;
+
+
+            Class1.Addition(out i);
+
+            
+            Console.WriteLine("The addition of the value is: {0}", i);
+            Console.ReadLine();
+        }
+    }
+}
