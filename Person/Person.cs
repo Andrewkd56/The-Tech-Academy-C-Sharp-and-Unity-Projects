4fa89ad00025701c9a332caa3592d287a409dc36
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-11-04 23:27:59 -0700

    Add files via upload

---
+++ Person/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine(fName + lName);
+            Console.ReadLine();
+        }
+    }
+}
