edd256ac5560732c07301d3ebf438373f61b0650
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-11-06 09:34:38 -0700

    Add files via upload

---
+++ Person/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        IQuittable quittable = new Employee();
+        public string fName { get; set; }
+        public string lName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine(fName + lName);
+            Console.ReadLine();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I quit");
+        }
+    }
+}
