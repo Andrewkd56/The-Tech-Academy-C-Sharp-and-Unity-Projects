c6bf3d826c6288cd54f1b328e21a94fd9fcef71d
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-11-07 20:36:17 -0800

    Add files via upload

---
+++ OperatorOverload/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee example = new Employee();
+
+            example.FirstName = "Kitty";
+            example.LastName = "Katz";
+            example.ID = 24923;
+
+            Employee example2 = new Employee();
+            example2.FirstName = "John";
+            example2.LastName = "Dudinsky";
+            example2.ID = 39292;
+
+            Console.ReadLine();
+        }
+    }
+}
