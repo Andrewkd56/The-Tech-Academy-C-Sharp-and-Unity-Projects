02e048b6065cbffa135e2a01dec86112d6b09383
Andrewkd56 <87885268+Andrewkd56@users.noreply.github.com>
2021-12-11 21:51:45 -0800

    Add files via upload

---
+++ Lambda/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public class Employee
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int ID { get; set; }
+        public Employee(string firstName, string lastName, int IDs)
+        {
+            firstname = firstName;
+            lastname = lastName;
+            ID = IDs;
+        }
+
+        
+    }
+}
